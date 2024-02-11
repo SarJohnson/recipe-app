9d06788783f848fd7d5ed113f8ee96db3d24e290
SarJohnson <sej17b@my.fsu.edu>
2024-02-10 20:44:19 -0500

    Updated media storage

--- recipe_app/settings.py
+++ recipe_app/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_URL='/login/'
